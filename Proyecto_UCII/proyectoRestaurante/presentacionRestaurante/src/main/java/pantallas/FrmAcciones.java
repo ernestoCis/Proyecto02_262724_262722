4720a3d100539eb454dfe59ca26103274880cf83
pauguece <paulinamichelgc@gmail.com>
2026-04-09 19:28:30 -0700

    botones de los modulos

--- Proyecto_UCII/proyectoRestaurante/presentacionRestaurante/src/main/java/pantallas/FrmAcciones.java
+++ Proyecto_UCII/proyectoRestaurante/presentacionRestaurante/src/main/java/pantallas/FrmAcciones.java
@@ -4,6 +4,7 @@
  */
 package pantallas;
 
+import componentes.BotonModulos;
 import controlador.Coordinador;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -57,7 +58,7 @@ public class FrmAcciones extends JFrame {
         fondo.setBackground(new Color(238, 238, 238));
 
         JPanel panelSuperior = new JPanel(new BorderLayout());
-        panelSuperior.setPreferredSize(new Dimension(820, 95));
+        panelSuperior.setPreferredSize(new Dimension(1000, 115));
         panelSuperior.setBackground(new Color(229, 171, 75));
 
         JPanel panelLogo = new JPanel();
@@ -65,10 +66,9 @@ public class FrmAcciones extends JFrame {
         panelLogo.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 10));
 
         lblLogo = new JLabel();
-        lblLogo.setPreferredSize(new Dimension(60, 60));
-
+        lblLogo.setPreferredSize(new Dimension(70, 70));
         ImageIcon logoOriginal = new ImageIcon("src\\main\\resources\\imagenes\\icono_restaurante.png");
-        Image logoEscalado = logoOriginal.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH);
+        Image logoEscalado = logoOriginal.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH);
         lblLogo.setIcon(new ImageIcon(logoEscalado));
 
         panelLogo.add(lblLogo);
@@ -77,7 +77,7 @@ public class FrmAcciones extends JFrame {
         panelTitulo.setOpaque(false);
 
         lblTitulo = new JLabel("Restaurante");
-        lblTitulo.setFont(new Font("SansSerif", Font.BOLD, 28));
+        lblTitulo.setFont(new Font("SansSerif", Font.BOLD, 36));
         lblTitulo.setForeground(new Color(45, 45, 45));
 
         panelTitulo.add(lblTitulo);
@@ -93,7 +93,7 @@ public class FrmAcciones extends JFrame {
         btnSalir.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
         ImageIcon salirOriginal = new ImageIcon("src\\main\\resources\\imagenes\\icono_salir.png");
-        Image salirEscalado = salirOriginal.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+        Image salirEscalado = salirOriginal.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH);
         btnSalir.setIcon(new ImageIcon(salirEscalado));
 
         panelSalir.add(btnSalir);
@@ -107,10 +107,10 @@ public class FrmAcciones extends JFrame {
         centro.setLayout(new BoxLayout(centro, BoxLayout.Y_AXIS));
         centro.setBorder(BorderFactory.createEmptyBorder(70, 0, 0, 0));
 
-        btnProductos = crearBotonRojo("PRODUCTOS", "src\\main\\resources\\imagenes\\icono_productos.png");
-        btnIngredientes = crearBotonRojo("INGREDIENTES", "src\\main\\resources\\imagenes\\icono_ingredientes.png");
-        btnClientes = crearBotonRojo("CLIENTES", "src\\main\\resources\\imagenes\\icono_clientes.png");
-        btnReportes = crearBotonRojo("REPORTES", "src\\main\\resources\\imagenes\\icono_reportes.png");
+        btnProductos = new BotonModulos("PRODUCTOS", "src\\main\\resources\\imagenes\\icono_productos.png");
+        btnIngredientes = new BotonModulos("INGREDIENTES", "src\\main\\resources\\imagenes\\icono_ingredientes.png");
+        btnClientes = new BotonModulos("CLIENTES", "src\\main\\resources\\imagenes\\icono_clientes.png");
+        btnReportes = new BotonModulos("REPORTES", "src\\main\\resources\\imagenes\\icono_reportes.png");
 
         btnProductos.setAlignmentX(CENTER_ALIGNMENT);
         btnIngredientes.setAlignmentX(CENTER_ALIGNMENT);
@@ -149,28 +149,4 @@ public class FrmAcciones extends JFrame {
         btnReportes.addActionListener(e -> System.out.println("Reportes"));
     }
 
-    private JButton crearBotonRojo(String texto, String rutaIcono) {
-        JButton boton = new JButton(texto);
-        boton.setFont(new Font("SansSerif", Font.PLAIN, 18));
-        boton.setBackground(new Color(204, 85, 78));
-        boton.setForeground(Color.WHITE);
-        boton.setOpaque(true);
-        boton.setBorderPainted(false);
-        boton.setContentAreaFilled(true);
-        boton.setFocusPainted(false);
-        boton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        boton.setHorizontalAlignment(SwingConstants.CENTER);
-        boton.setPreferredSize(new Dimension(280, 60));
-        boton.setMaximumSize(new Dimension(280, 60));
-        boton.setMinimumSize(new Dimension(280, 60));
-        boton.setBorder(BorderFactory.createLineBorder(new Color(140, 55, 50), 2));
-        boton.setMargin(new Insets(10, 20, 10, 10));
-        boton.setIconTextGap(18);
-
-        ImageIcon iconoOriginal = new ImageIcon(rutaIcono);
-        Image iconoEscalado = iconoOriginal.getImage().getScaledInstance(26, 26, Image.SCALE_SMOOTH);
-        boton.setIcon(new ImageIcon(iconoEscalado));
-
-        return boton;
-    }
 }
